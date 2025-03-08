a4b77af2eeca404e188a47b51842a8d326b3a747
LamJIson <150525268+LamJIson@users.noreply.github.com>
2025-03-08 10:47:01 +0800

    Update kaoshibao.js

--- Script/kaoshibao.js
+++ Script/kaoshibao.js
@@ -1,186 +1,37 @@
-{
-  "code": "200",
-  "data": {
-    "icon": {
-      "level_2": [
-        {
-          "mini_hide": "1",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2ad.png",
-          "name": "学员练习无广告",
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2ad.png"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2kf.png?20221222",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2kf.png",
-          "name": "专属在线客服",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2dy.png?1",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2dy.png",
-          "name": "组卷导出打印",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2ks.png?12222",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2ks.png",
-          "name": "创建100场考试",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2zb.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2zb.png",
-          "name": "考试防作弊",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2dc.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2dc.png",
-          "name": "导出考生试卷",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2zl.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2zl.png",
-          "name": "上传学习资料",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2qz.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2qz.png",
-          "name": "创建10个群组",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v2tp.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v2tp.png",
-          "name": "分享图片设置",
-          "mini_hide": "1"
-        }
-      ],
-      "document_detail": [
-        {
-          "name": "VIP专享文档",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_doc.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "VIP精编题库",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_paper.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "VIP搜题权益",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_so.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "无广告",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_ad.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "赠送金币",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_jinbi.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "易错题",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_cuoti.png",
-          "mini_hide": "1"
-        }
-      ],
-      "level_1": [
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/hyfw_2.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/b/soti.png",
-          "name": "VIP搜题权益",
-          "mini_hide": "1"
-        },
-        {
-          "mini_hide": "1",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1ad.png",
-          "name": "去除广告",
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1ad.png"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1gp.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1gp.png",
-          "name": "易错题",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1lx.png?20221222",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1lx.png?20221222",
-          "name": "离线练习",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1hb.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1hb.png",
-          "name": "合并题库",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1jm.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1jm.png",
-          "name": "题库加密",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1qr.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1qr.png",
-          "name": "千人千卷",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1zj.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1zj.png",
-          "name": "手工组卷",
-          "mini_hide": "1"
-        },
-        {
-          "banner": "https://resource.zaixiankaoshi.com/mini/v/b/v1xxlx.png",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1xxlx.png",
-          "name": "选项乱序",
-          "mini_hide": "1"
-        }
-      ],
-      "mini_document_detail": [
-        {
-          "name": "VIP专享文档",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_doc.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "VIP精编题库",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_paper.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "VIP搜题权益",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_so.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "千人千卷",
-          "icon": "https://resource.zaixiankaoshi.com/mini/v/v1qr.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "赠送金币",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_jinbi.png",
-          "mini_hide": "1"
-        },
-        {
-          "name": "易错题",
-          "icon": "https://resource.zaixiankaoshi.com/mini/vip_cuoti.png",
-          "mini_hide": "1"
-        }
-      ]
-    }
-  },
-  "time": "1741367748",
-  "encrypt": "Kv2cFu5WHpwE8nnNzftb5T=="
+// 获取原始响应的 body
+const body = $response.body ? JSON.parse($response.body) : {};
+
+// 定义一个函数，用于过滤掉 mini_hide 为 "1" 的图标
+const filterIcons = (icons) => {
+  return icons.filter(item => item.mini_hide !== "1");
+};
+
+// 遍历 data.icon 下的所有数组，过滤掉 mini_hide 为 "1" 的图标
+if (body.data && body.data.icon) {
+  const iconData = body.data.icon;
+
+  // 过滤 level_2
+  if (iconData.level_2 && Array.isArray(iconData.level_2)) {
+    iconData.level_2 = filterIcons(iconData.level_2);
+  }
+
+  // 过滤 document_detail
+  if (iconData.document_detail && Array.isArray(iconData.document_detail)) {
+    iconData.document_detail = filterIcons(iconData.document_detail);
+  }
+
+  // 过滤 level_1
+  if (iconData.level_1 && Array.isArray(iconData.level_1)) {
+    iconData.level_1 = filterIcons(iconData.level_1);
+  }
+
+  // 过滤 mini_document_detail
+  if (iconData.mini_document_detail && Array.isArray(iconData.mini_document_detail)) {
+    iconData.mini_document_detail = filterIcons(iconData.mini_document_detail);
+  }
 }
 
+// 返回修改后的响应
+$done({
+  body: JSON.stringify(body) // 将修改后的 body 转换为 JSON 字符串
+});
