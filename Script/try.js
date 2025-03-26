86624de0da4d0180c02f4e0769b8e3d608727c46
LamJIson <150525268+LamJIson@users.noreply.github.com>
2025-03-26 10:32:06 +0800

    Update try.js

--- Script/try.js
+++ Script/try.js
@@ -2,8 +2,8 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 // Modify the response to set the expiration times to the end of 2099
-obj.data.try_expire_time = "2066-12-31 23:59:59";
-obj.data.vip_expire_time = "2066-12-31 23:59:59";
+obj.data.try_expire_time = "2099-12-31 23:59:59";
+obj.data.vip_expire_time = "2099-12-31 23:59:59";
 obj.data.can_try = true; // Optionally, set can_try to true if needed
 
 $done({body: JSON.stringify(obj)});
