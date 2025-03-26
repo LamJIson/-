c788018faa25b0c9dc56f6786355e1ca990081e0
LamJIson <150525268+LamJIson@users.noreply.github.com>
2025-03-26 10:26:50 +0800

    Update bcz.js

--- Script/bcz.js
+++ Script/bcz.js
@@ -7,7 +7,7 @@ obj.data.vip_expire = "2066-12-31 23:59:59"; // Set the expiration date to the e
 obj.data.newer_discount_expire = "2099-12-31 23:59:59"; // Set the newer discount expiration date to the end of 2099
 obj.data.is_try_vip = 1; // Set the try VIP status to 1
 obj.data.try_vip_expire = "2066-12-31 23:59:59"; // Set the try VIP expiration date to the end of 2099
-obj.data.vip_days = 365; // Set the VIP days to 365
+obj.data.vip_days = 999; // Set the VIP days to 365
 
 $done({body: JSON.stringify(obj)});
 
